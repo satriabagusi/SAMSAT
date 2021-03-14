1f728c56ab142647c921616412f94ab666f142a0
Satria Bagus <satriabagus@Satrias-iMac.local>
2021-03-15 00:15:27 +0700

    designing with template

--- App.js
+++ App.js
@@ -1,26 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { configureFonts, DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { useFonts } from 'expo-font';
+import AppLoading  from 'expo-app-loading';
 import Router from './Router';
+
 
 const theme = {
   ...DefaultTheme,
   roundness: 2,
   colors: {
     ...DefaultTheme.colors,
-    primary: '#3498db',
-    accent: '#f1c40f',
+    primary: '#F99053',
+    accent: '#DC7E47',
   },
 };
 
-export default function App() {
-  return (
-    <PaperProvider theme={theme}>
-      {/* <SafeAreaView> */}
+export default App => {
+  let [fontsLoaded] = useFonts({
+    'Montserrat-Regular': require('./assets/fonts/Montserrat-Regular.ttf'),
+    'Montserrat-Medium': require('./assets/fonts/Montserrat-Medium.ttf'),
+    'Montserrat-Light': require('./assets/fonts/Montserrat-Light.ttf'),
+    'Montserrat-Bold': require('./assets/fonts/Montserrat-Bold.ttf'),
+    'Montserrat-Thin': require('./assets/fonts/Montserrat-Thin.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading/>
+  }else{
+    return(
+      <PaperProvider theme={theme}>
         <Router />
-      {/* </SafeAreaView> */}
-    </PaperProvider>
-  );
+      </PaperProvider>
+    );
+  }
 }
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,39 +1,65 @@
 import React, { Component } from 'react';
-import { FlatList, Platform, SafeAreaView, StyleSheet, View } from 'react-native';
-import { Surface, Text, Button, Divider } from 'react-native-paper';
-import { responsiveScreenWidth, responsiveScreenFontSize, responsiveScreenHeight, responsiveWidth } from 'react-native-responsive-dimensions';
+import { FlatList, ImageBackground, Platform, SafeAreaView, StyleSheet, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Surface, Text, Button, Divider, ActivityIndicator, useTheme } from 'react-native-paper';
+import { responsiveScreenWidth, responsiveScreenFontSize, responsiveScreenHeight, responsiveWidth, responsiveFontSize } from 'react-native-responsive-dimensions';
+import { useEffect, useState } from 'react';
+import { color } from 'react-native-reanimated';
 
 
-const DATA = [
-    {
-        title: 'First Item',
-    },
-    {
-        title: 'Second Item',
-    },
-    {
-        title: 'Third Item',
-    },
-];
+function HomeScreen(props){
+    const { colors } = useTheme(); 
+    const [isLoading, setLoading] = useState(true);
+    const [news, setNews] = useState([]);
 
-function HomeScreen(){
+    useEffect(() => {
+        fetch('https://newsapi.org/v2/everything?q=indramayu&apiKey=6f0e7d3efe8a4836a0d7621f2c324e05&sortBy=popularity&pageSize=3')
+        .then((response) => response.json())
+        .then((json) => setNews(json.articles))
+        .catch((error) => console.error(error))
+        .finally(() => setLoading(false))
+    }, []);
     
     return(
-        <SafeAreaView style={styles.container}>
-            <View style={styles.headSection}>
-            <FlatList
-            showsHorizontalScrollIndicator={false}
-            horizontal={true}
-            data={DATA}
-            renderItem={({ item }) => (
-                <Surface style={styles.btnMenu}>
-                        <Text>{item.title}</Text>
-                    </Surface>
-                )}
-                />
+        <View style={styles.container}>
+            <StatusBar style="light" />
+            <ImageBackground source={require('../assets/header.png')} style={{width: responsiveScreenWidth(105), zIndex: 999}}>
+                <View style={{height: responsiveScreenHeight(20), alignItems: 'center', justifyContent: 'center'}}>
+                    <Text style={{color: '#FFF', fontSize: responsiveFontSize(3), fontFamily: 'Montserrat-Bold'}}>SAMSAT INDRAMAYU</Text>
+
+                </View>
+            </ImageBackground>
+
+            <View style={{backgroundColor: '#FFFFFF', width: responsiveScreenWidth(100), height: responsiveScreenHeight(30), marginTop: -50, borderRadius: 15}}>
+
             </View>
 
-        </SafeAreaView>
+            <View style={{backgroundColor: '#FFFFFF', width: responsiveScreenWidth(100), height: responsiveScreenHeight(30), marginTop: 25, borderTopEndRadius: 15, borderTopStartRadius: 15}}>
+                <Text style={{marginTop:25, marginLeft: 20,fontFamily: 'Montserrat-Bold'}}>Berita dan Info</Text>
+                <View style={styles.headSection}>
+                    { news?
+                    <FlatList
+                    showsHorizontalScrollIndicator={false}
+                    horizontal={true}
+                    data={news}
+                    renderItem={({ item }) => (
+                        <Surface style={styles.btnMenu}>
+                            <ImageBackground style={{width: responsiveScreenWidth(85), height: responsiveScreenHeight(20), borderRadius: 10}}
+                            source={ {uri:  item.urlToImage} }>
+                                <Text style={{fontFamily: 'Montserrat-Bold', fontSize: 20, marginTop: responsiveScreenHeight(12), marginHorizontal: responsiveScreenWidth(2), color: '#FFF'}}>{item.title}</Text>
+                            </ImageBackground>
+                        </Surface>
+                        )}
+                        /> :
+                         <ActivityIndicator animating={true} color={color.primary}/>
+                        }
+                    
+                </View>
+            </View>
+
+            
+
+        </View>
     );
 }
 
@@ -42,12 +68,11 @@ export default HomeScreen;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        marginHorizontal: 10,
-        marginTop: Platform.OS === 'android' ? responsiveScreenHeight(4) : 0
+        // marginTop: Platform.OS === 'android' ? responsiveScreenHeight(4) : 0
     },
     headSection: {
         height: responsiveScreenHeight(25),
-        marginTop: responsiveScreenWidth(25)
+        marginTop: responsiveScreenWidth(5)
     },
     btnMenu: {
         padding: 8,
@@ -55,7 +80,9 @@ const styles = StyleSheet.create({
         width: responsiveScreenWidth(85),
         alignItems: 'center',
         justifyContent: 'center',
-        elevation: 3,
+        elevation: 0,
         marginHorizontal: responsiveWidth(2),
+        backgroundColor: '#ffc031',
+        borderRadius: 10
     },
 });
